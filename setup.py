6c80735ffac5fd58011adf987a2ab8028f56a1f3
Carl Backstrom <genzorg@gmail.com>
2014-01-28 20:05:06 +0100

    Added setup script
    fixes #5

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup( name="warp",
+      version='1.0.0',
+      options = {},
+      description='Standardlib',
+      author='Carl Backstrom',
+      packages=find_packages()
+    #   install_requires=[
+    #     "requirement >= version_number",
+    # ],
+
+)
